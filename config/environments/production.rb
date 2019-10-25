3dc3d6574172bb0a5b9637f108577cb2db7dc300
uehara-delta <uehara.delta@gmail.com>
2019-10-26 08:56:01 +0900

    本番環境でのメール送信設定の誤記を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     address:              'smtp.sendgrid.net',
     port:                 587,
     authentication:       :plain,
-    user_name:            ENV['SENDGRID_USER_NAME'],
+    user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
     domain:               'heroku.com',
     enable_starttls_auto: true
