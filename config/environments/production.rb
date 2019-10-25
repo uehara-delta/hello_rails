ebe442104193598c5219f77c3797987afe35ebc0
uehara-delta <uehara.delta@gmail.com>
2019-10-25 16:32:32 +0900

    本番環境でのメール送信設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
     authentication:       :plain,
     user_name:            ENV['SENDGRID_USER_NAME'],
     password:             ENV['SENDGRID_PASSWORD'],
-    domain:               'heroku.com'
+    domain:               'heroku.com',
     enable_starttls_auto: true
   }
 
